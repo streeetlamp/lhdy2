c0ecdccbff5fe4c8ead131d1e1b6b06baab794f2
Cody W <cody@codywhitby.net>
2015-01-05 15:28:12 -0500

    example.js is a placeholder file so the libs dir will be tracked in the repo

---
+++ app/js/scripts/libs/example.js
@@ -0,0 +1,8 @@
+/*
+Everything in this /scripts directory will be 
+magically concatenated together. This is just
+a placeholder file so the directory can be 
+tracked with git. 
+
+Delete this if you want, or don't.
+*/
